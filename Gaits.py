1999095bca229d7ae90055232e9470c3ae48c57b
TypePark <thetypepark@gmail.com>
2026-06-03 19:58:06 +0300

    Moved constants to init.

--- Gaits.py
+++ Gaits.py
@@ -6,15 +6,14 @@ class Gaits:
         self.hexapod = hexapod
         self.leg_data_dict = parser.leg_data_dict
         self.leg_move_order = parser.gait_selector(gait_type)
-
+        self.gait_speed = 2.0
+        self.total_cycle = math.pi * 2
+        self.num_legs = len(self.leg_move_order)
 
     def hexapod_tripod_gait(self, current_time):
-        gait_speed = 2.0
-        total_cycle = 2 * math.pi
-
         for index, leg_id in enumerate(self.leg_move_order):
             legs_used_first_cycle = math.pi if index >= 3 else 0.0
-            each_cycle = (current_time * gait_speed + legs_used_first_cycle) % total_cycle
+            each_cycle = (current_time * self.gait_speed + legs_used_first_cycle) % self.total_cycle
             polarity = -1.0 if "l_" in leg_id else 1.0
             if each_cycle < math.pi:
                 leg_target = self.leg_data_dict[leg_id]["coxa"]["center"] + (math.sin(each_cycle) * polarity) / 5
@@ -28,13 +27,9 @@ class Gaits:
             p.setJointMotorControl2(self.hexapod, self.leg_data_dict[leg_id]["foot"]["index"], p.POSITION_CONTROL, foot_target)
 
     def hexapod_ripple_gait(self, current_time):
-        num_legs = len(self.leg_move_order)
-        gait_speed = 2.0
-        total_cycle = 2 * math.pi
-
         for index, leg_id in enumerate(self.leg_move_order):
-            phase_time = (total_cycle / num_legs) * index
-            each_cycle = (current_time * gait_speed + phase_time) % total_cycle
+            phase_time = (self.total_cycle / self.num_legs) * index
+            each_cycle = (current_time * self.gait_speed + phase_time) % self.total_cycle
             polarity = -1.0 if "l_" in leg_id else 1.0
             if each_cycle < phase_time:
                 leg_target = self.leg_data_dict[leg_id]["coxa"]["center"] + math.sin(each_cycle) * polarity / 5
@@ -47,14 +42,11 @@ class Gaits:
             p.setJointMotorControl2(self.hexapod, self.leg_data_dict[leg_id]["foot"]["index"], p.POSITION_CONTROL, targetPosition=foot_target)
 
     def hexapod_wave_gait(self, current_time):
-        num_legs = len(self.leg_move_order)
-        gait_speed = 1.0
-        total_cycle = 2 * math.pi
-        phase_time = total_cycle / num_legs
-        stance_time = total_cycle - phase_time
+        phase_time = self.total_cycle / self.num_legs
+        stance_time = self.total_cycle - phase_time
 
         for index, leg_id in enumerate(self.leg_move_order):
-            each_cycle = (current_time * gait_speed + (phase_time * index)) % total_cycle
+            each_cycle = (current_time * self.gait_speed + (phase_time * index)) % self.total_cycle
             polarity = -1.0 if "l_" in leg_id else 1.0
 
             if each_cycle < phase_time:
